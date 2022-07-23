9cae2301edbc9ff40e3b48cac305b85d2871c52d
allan turpin <allan974.turpin@gmail.com>
2022-07-24 00:31:05 +0200

    Gestion des rôles user et admin

--- paradis/routes/web.php
+++ paradis/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+################################ Route pour ma page accueil  ########################
 Route::get('/', function () {
-    return view('welcome');
+    return view('paradis.accueil');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+Route::get('/accueil', function () {
+    return view('paradis.accueil');
+})->middleware(['auth'])->name('accueil');
+
+#########################################  Route vers le panel admistrateur ###################
+Route::middleware(['auth', 'admin'])->name('admin.')->prefix('admin')->group(function() {
+    Route::get('/', [AdminController::class, 'index'])->name('index');
+});
 
 require __DIR__.'/auth.php';
