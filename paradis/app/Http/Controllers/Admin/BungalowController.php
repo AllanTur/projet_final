13f7c42d5449a6756adcf4e45116ebc15ca4f78d
allan turpin <allan974.turpin@gmail.com>
2022-07-24 23:27:30 +0200

    Modification du font de la Panel Admin : Version 2

--- paradis/app/Http/Controllers/Admin/BungalowController.php
+++ paradis/app/Http/Controllers/Admin/BungalowController.php
@@ -26,7 +26,7 @@ class BungalowController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.bungalows.create');
     }
 
     /**
--- paradis/resources/views/admin/bungalows/index.blade.php
+++ paradis/resources/views/admin/bungalows/index.blade.php
@@ -1,67 +1,60 @@
 @extends('layouts.adminlayouts.admin')
 
 @section('content')
-{{-- width: 95%; --}}
-<div class="mx-auto" style="margin-top: 5%;">
-    <table class="table table-dark table-hover">
-        <thead>
-            <tr>
-                <th scope="col">Id</th>
-                <th scope="col">Nom</th>
-                <th scope="col">Description</th>
-                <th scope="col">Image</th>
-                <th scope="col">prix (€)</th>
-                <th scope="col">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>blabla</td>
-                <td>PHOTO</td>
-                <td>150</td>
-                <td>
+<div class="dash-content">
+    <div class="activity">
+        <div class="title">
+            <span class="text">Liste des Bungalows</span>
+            <button style="margin-left: 10px" type="button" class="btn btn-outline-primary"><a href="{{ route('admin.bungalows.create') }}">Ajouter</a></button>
+        </div>
+
+        <div class="activity-data">
+            <div class="data names">
+                <span class="data-title">Name</span>
+                <span class="data-list">Prem Sahi</span>
+                <span class="data-list">Prem Sahi</span>
+                <span class="data-list">Prem Sahi</span>
+            </div>
+            <div class="data email">
+                <span class="data-title">E-mail</span>
+                <span class="data-list">test@gmail.com</span>
+                <span class="data-list">test@gmail.com</span>
+                <span class="data-list">test@gmail.com</span>
+            </div>
+            <div class="data joined">
+                <span class="data-title">Date</span>
+                <span class="data-list">01-02-2022</span>
+                <span class="data-list">01-02-2022</span>
+                <span class="data-list">01-02-2022</span>
+            </div>
+            <div class="data type">
+                <span class="data-title">Type</span>
+                <span class="data-list">New</span>
+                <span class="data-list">New</span>
+                <span class="data-list">New</span>
+            </div>
+            <div class="data status">
+                <span class="data-title">Status</span>
+                <span class="data-list">
                     <div class="btn-group" role="group" aria-label="Basic outlined example">
                         <button type="button" class="btn btn-outline-primary">Modifier</button>
                         <button type="button" class="btn btn-outline-danger">Supprimer</button>
-
                     </div>
-
-                </td>
-            </tr>
-
-            <tr>
-                <th scope="row">2</th>
-                <td>Mark</td>
-                <td>blabla</td>
-                <td>PHOTO</td>
-                <td>150</td>
-                <td>
+                </span>
+                <span class="data-list">
                     <div class="btn-group" role="group" aria-label="Basic outlined example">
                         <button type="button" class="btn btn-outline-primary">Modifier</button>
                         <button type="button" class="btn btn-outline-danger">Supprimer</button>
-
                     </div>
-                </td>
-            </tr>
-
-            <tr>
-                <th scope="row">3</th>
-                <td>Mark</td>
-                <td>blabla</td>
-                <td>PHOTO</td>
-                <td>150</td>
-                <td>
+                </span>
+                <span class="data-list">
                     <div class="btn-group" role="group" aria-label="Basic outlined example">
                         <button type="button" class="btn btn-outline-primary">Modifier</button>
                         <button type="button" class="btn btn-outline-danger">Supprimer</button>
-
                     </div>
-                </td>
-            </tr>
-
-        </tbody>
-    </table>
+                </span>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
